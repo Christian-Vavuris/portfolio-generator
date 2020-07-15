213917c5ac7193011232ada2822dce412f5bb668
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-07-14 22:21:06 -0700

    added in dynamic HTML footer

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const [name, github] = profileDataArgs;
 
 
 
 
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//     if (err) throw err;
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+
 
 const inquirer = require ('inquirer');
 
@@ -113,6 +110,12 @@ const promptProject = portfolioData => {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-      console.log(portfolioData);
-  })
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
 
